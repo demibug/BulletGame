c80d5258d3dbdd046d12dea2f1d312ffe6266ab3
wanghaiji <wanghaiji@163.com>
2024-11-29 13:48:26 +0800

    terrain system

---
+++ UnityProject/Assets/ThirdParty/TerrainGridSystem/Scripts/TGSConfigEntry.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+
+namespace TGS {
+
+    [Serializable]
+    public struct TGSConfigEntry {
+        public bool visible;
+        public int territoryIndex;
+        public Color color;
+        public int textureIndex;
+        public int tag;
+        public bool canCross;
+        public float crossCost; // all sides
+        public float[] crossSidesCost; // per side
+        public Vector2 textureScale, textureOffset;
+        public TGSConfigAttribPair[] attribData;
+    }
+
+
+    [Serializable]
+    public struct TGSConfigAttribPair {
+        public string key;
+        public string stringValue;
+        public float numericValue;
+    }
+
+}
